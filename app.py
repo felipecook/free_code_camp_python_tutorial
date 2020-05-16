0f649b7d7ea964b6a81c2137b7c8f768d2749108
Felipe Cook <felipepcook@gmail.com>
2020-05-16 10:47:25 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from math import *
+
+
+# prints the phrase
+phrase = "Cook Academy"
+print(phrase)
+
+
+# gives index of first instance of that value
+phrase = "Cook Academy"
+print(phrase.index("A"))
+
+
+# replaces the designated target string with new string
+phrase = "Cook Academy"
+print(phrase.replace("Cook", "Angry"))
+
+
+# Gives absolute value
+my_num = -5
+print(abs(my_num))
+
+
+# give a number and the power for that number
+print(pow(3, 2))
+
+
+# returns the larger of the two numbers
+print(max(3, 2))
+
+
+# returns the smaller of the two numbers
+print(min(3, 2))
+
+
+# rounds the number
+print(round(3.7))
+
+
+# rounds down always
+print(floor(3.99))
+
+
+# rounds up always
+print(ceil(3.01))
+
+
+# returns square root of number
+print(sqrt(9))
+
